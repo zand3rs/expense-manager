a018894426ed8afdcfb44cfc748b1234ffd64a0c
zander <aamagtipon@voyagerinnovation.com>
2018-07-20 13:45:30 +0800

    updated routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET categories listing. */
+//-- index
 router.get('/', function(req, res, next) {
   res.send('categories');
+});
+
+//-- show
+router.get('/:id', function(req, res, next) {
+  res.send('show category: ' + req.param('id'));
+});
+
+//-- edit
+router.get('/:id/edit', function(req, res, next) {
+  res.send('edit category: ' + req.param('id'));
+});
+
+//-- create
+router.post('/', function(req, res, next) {
+  res.send('create categories');
+});
+
+//-- update
+router.put('/:id', function(req, res, next) {
+  res.send('update category: ' + req.param('id'));
+});
+
+//-- destroy
+router.delete('/:id', function(req, res, next) {
+  res.send('destroy category: ' + req.param('id'));
 });
 
 module.exports = router;
--- routes/expenses.js
+++ routes/expenses.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET expenses listing. */
+//-- index
 router.get('/', function(req, res, next) {
   res.send('expenses');
+});
+
+//-- show
+router.get('/:id', function(req, res, next) {
+  res.send('show expense: ' + req.param('id'));
+});
+
+//-- edit
+router.get('/:id/edit', function(req, res, next) {
+  res.send('edit expense: ' + req.param('id'));
+});
+
+//-- create
+router.post('/', function(req, res, next) {
+  res.send('create expenses');
+});
+
+//-- update
+router.put('/:id', function(req, res, next) {
+  res.send('update expense: ' + req.param('id'));
+});
+
+//-- destroy
+router.delete('/:id', function(req, res, next) {
+  res.send('destroy expense: ' + req.param('id'));
 });
 
 module.exports = router;
