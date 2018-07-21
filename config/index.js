324020faea173dcf70884e736f7f0bd197ecd1cf
zander <aamagtipon@voyagerinnovation.com>
2018-07-21 11:23:48 +0800

    added config

---
+++ config/index.js
@@ -0,0 +1,20 @@
+/*
+ * Main config
+ *
+ */
+
+//==============================================================================
+//-- export
+
+module.exports = {
+
+  mysql: {
+    host: "localhost",
+    user: "expense_manager",
+    password: "expense_manager",
+    database: "expense_manager"
+   }
+
+};
+
+//==============================================================================
